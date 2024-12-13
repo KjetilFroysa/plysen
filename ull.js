2b51889bda22a769bdd8c838338937f3023a7f42
Kjetil <kjetilfroysa@gmail.com>
2024-12-13 15:32:45 +0100

    Formatert kvaliteter

--- ull.js
+++ ull.js
@@ -1,7 +1,36 @@
-const knapp = document.getElementsByClassName('regn-ut')
+const knapp = document.getElementById('regn-ut');
 
-knapp.addEventListener('click', () => {
-    const kvalitet = document.getElementsByID('kvalitet').value();
-    const mengde = document.getElementsByID('mengde').value();
+knapp.addEventListener("click", function(e) {
+    const kvalitet = document.getElementById('kvalitet').value;
+    const mengde = document.getElementById('mengde').value;
     console.log(kvalitet + mengde);
 });
+
+
+class HIFA {
+    constructor(navn, nummer, c1_kvit, c1_svart, c1_brun_620) {
+        this.navn = navn;
+        this.nummer = nummer;
+        this.oppskrift = [
+            c1_kvit,
+            c1_svart,
+            c1_brun_620,
+        ]
+    }
+};
+
+const hifa_6054 = new HIFA ("HIFA", 6054, 97.5, 2.5, 0);
+const hifa_6055 = new HIFA ("HIFA", 6055, 88, 12, 0);
+
+fetch('./kvaliteter.json')
+.then(response => response.json())
+.then(data=> {
+    console.log(data);
+});
+
+console.log(data.name);
+
+console.log (hifa_6054);
+console.log(hifa_6054.oppskrift);
+console.log (hifa_6055);
+console.log(hifa_6055.oppskrift[0]);
