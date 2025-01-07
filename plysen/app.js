670421ad94d6bd55f7daa76614f3b760f8756305
Kjetil <kjetilfroysa@gmail.com>
2025-01-07 15:31:58 +0100

    fikset kart og fikset morgan manglet

--- plysen/app.js
+++ plysen/app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 const bodyParser = require('body-parser');
 
 
@@ -20,7 +19,6 @@ app.set('view engine', 'jade');
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
